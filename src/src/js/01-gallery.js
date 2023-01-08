5040b4fa297ead88846b204b84ecce1b94828641
KamilP102 <kamilp1319@gmail.com>
2023-01-08 20:16:02 +0100

    Update 01-gallery.js

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery")
+const imageList = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", imageList);
+preventDefault();
