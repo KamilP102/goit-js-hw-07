d57972270ba3fd5556740664b6446e4fbe4d782d
KamilP102 <kamilp1319@gmail.com>
2023-01-23 15:29:24 +0100

    Done task 07

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const imageList = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      alt=${item.description}
+    />
+  </a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", imageList);
+let lightbox = new SimpleLightbox(".gallery a", {
+  sourceAttr: `href`,
+  captionsData: `alt`,
+  captionDelay : 250,
+});
